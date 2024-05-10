803d35239f60934a0d068928c06bbddba7b88e45
«Adil786000» <ai9069935@gmail.com>
2024-05-10 11:48:04 +0200

    Luego he creado los constructores

--- elmento.java
+++ elmento.java
@@ -2,3 +2,13 @@ public class Elemento {
     // Atributos
     private String nombre;
     private String descripción;
+
+    // Constructor
+    public Elemento(String nombre, String descripción) {
+        this.nombre = nombre;
+        this.descripción = descripción;
+    }
+
+    
+}
+
