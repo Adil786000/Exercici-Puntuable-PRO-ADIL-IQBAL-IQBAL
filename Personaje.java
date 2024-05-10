3d4ce47a97f8ec3bec527847b1e23a6745608d70
«Adil786000» <ai9069935@gmail.com>
2024-05-10 10:40:43 +0200

    Luego he creado la primera classe que se llama personaje y dentro de esa classe he añadido los atributos necessarios.

---
+++ Personaje.java
@@ -0,0 +1,10 @@
+public class Personaje {
+    // Atributos
+    private String nombre;
+    private int niveles;
+    private int puntosDeVida;
+    private int puntosDeMana;
+    private String arma;
+    private String armadura;
+
+}
