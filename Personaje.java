4eb03a9fa69983e2f0c7e3f89f89b62f7c6b1159
«Adil786000» <ai9069935@gmail.com>
2024-05-10 10:46:23 +0200

    Aquí ahora he creado los constructores de los artibutos.

--- Personaje.java
+++ Personaje.java
@@ -7,4 +7,15 @@ public class Personaje {
     private String arma;
     private String armadura;
 
+    //Constructor
+    public Personaje(String nombre, int niveles, int puntosDeVida, int
+    puntosDeMana, String arma, String armadura) {
+        this.nombre = nombre;
+        this.niveles = niveles;
+        this.puntosDeVida = puntosDeVida;
+        this.puntosDeMana = puntosDeMana;
+        this.arma = arma;
+        this.armadura = armadura;
+    }
+  
 }
