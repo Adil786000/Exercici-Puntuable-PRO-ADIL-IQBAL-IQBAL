7858311c3976a6d1e8eb67cd8fc809b97d4c8620
«Adil786000» <ai9069935@gmail.com>
2024-05-10 11:55:11 +0200

    dentro de eso he crado los prints.

--- main.java
+++ main.java
@@ -3,3 +3,15 @@ public class Main {
         File file = new File("personajes.xml");
         Personaje personaje = Personaje.fromXML(file);
 
+        if (personaje != null) {
+            System.out.println("Nombre: " + personaje.getNombre());
+            System.out.println("Niveles: " + personaje.getNiveles());
+            System.out.println("Puntos de vida: " + personaje.getPuntosDeVida());
+            System.out.println("Puntos de mana: " + personaje.getPuntosDeMana());
+            System.out.println("Arma: " + personaje.getArma());
+            System.out.println("Armadura: " + personaje.getArmadura());
+        } else {
+            System.out.println("No se encontró un personaje en el archivo XML");
+        }
+    }
+}
